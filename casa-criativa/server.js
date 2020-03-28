38ffdb723c3c6385aa6738d020b0d783eee07f82
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-03-28 15:56:40 -0300

    Configurando servidor

--- casa-criativa/server.js
+++ casa-criativa/server.js
@@ -1,1 +1,12 @@
-console.log("Exercutando pelo npm");
+// usei o express para criar e configurar meu servidor
+const express = require("express");
+const server = express();
+
+// criei uma rota /
+// e capturo o pedido do cliente para responder
+server.get("/", function(req, res) {
+    return res.send("Resposta do servidor");
+});
+
+// liguei o servidor na porta 3000
+server.listen(3000);
