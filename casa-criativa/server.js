e694f74075ae7ae596dcc68d73e623f584fb9077
Anderson Leite <anderson.leite.bsi@gmail.com>
2020-03-31 18:22:20 -0300

    Refatorando ultimas ideias

--- casa-criativa/server.js
+++ casa-criativa/server.js
@@ -2,6 +2,37 @@
 const express = require("express");
 const server = express();
 
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cursos de programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum esse quibusdam ratione.",
+        url: "http://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "Karaokê",
+        category: "Diversão",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum esse quibusdam ratione.",
+        url: "https://www.tvkaraoke.com.br/"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum esse quibusdam ratione.",
+        url: "https://www.personare.com.br/meditacao"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum esse quibusdam ratione.",
+        url: "https://www.exercicioemcasa.com.br/"
+    }
+]
+
 // configurar arquivos estaticos(css, scripts, imagens)
 server.use(express.static("public"));
 
@@ -9,16 +40,29 @@ server.use(express.static("public"));
 const nunjucks = require("nunjucks");
 nunjucks.configure("views", {
     express: server,
+    noCache: true,
 });
 
 // criei uma rota /
 // e capturo o pedido do cliente para responder
 server.get("/", function(req, res) {
-    return res.render("index.html");
+
+    const reversedIdeas = [...ideas].reverse();
+
+    let lastIdeas = [];
+    for (let idea of reversedIdeas) {
+        if(lastIdeas.length < 2) {
+            lastIdeas.push(idea);
+        }
+    }
+
+    return res.render("index.html", { ideas: lastIdeas });
 });
 
 server.get("/ideias", function(req, res) {
-    return res.render("ideias.html");
+
+    const reversedIdeas = [...ideas].reverse();
+    return res.render("ideias.html", { ideas: reversedIdeas });
 });
 
 // liguei o servidor na porta 3000
